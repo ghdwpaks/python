bc66aabb951615409b4fdfeec808def86152ed06
ghdwpaks <hjm7904@gmail.com>
2021-02-04 21:26:44 +0900

    how to print somthing at python

--- day01/ex01.py
+++ day01/ex01.py
@@ -1,1 +1,17 @@
-Hello here is exam01
+# print("Hello world!");
+# print('Hello world!');
+
+print("Hello world!");
+print('Hello world!');
+#ctrl + / 
+#위에 키를 누르면 선택한 모든 줄을 주석처리시킬 수 있다.
+
+
+# Ctrl + alt + n : vs code 내부 cmd 에서의 코드실행
+#은 한줄 주석이다. C/C++ , Java의 //과 같은 주석 시스템인듯하다
+'''
+여러줄을 주석처리로 하려면 이렇게 홀따옴표 3개씩 찍어서
+주석의 시작과 끝마침을 의미한다.
+자바와 C/C++의 /* */과 같다.
+'''
+

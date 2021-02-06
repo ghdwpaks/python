047ce6876544ffed47ee5ff7ae5881b092d5d8be
ghdwpaks <hjm7904@gmail.com>
2021-02-06 20:54:16 +0900

    how to cast obj at py

--- day02/housework/ex01.py
+++ day02/housework/ex01.py
@@ -4,3 +4,17 @@ a = int(input())
 print("a :",a)
 
 
+print("b\n>>", end='')
+b = input()
+print("a :",a)
+b = int(b)
+print("a+b = ", a+b)
+print("\n분기점\n")
+c = a+b
+print("c : ", c)
+print("\n분기점\n")
+a = int(a)
+print("a :" ,a)
+b = int(b)
+print("b : ", b)
+print("a+b : ",a+b)

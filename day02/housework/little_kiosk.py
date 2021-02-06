a66737e14efd4020e0444cb5d607dc81e359ad13
ghdwpaks <hjm7904@gmail.com>
2021-02-06 23:10:07 +0900

    made little kiosk part 1 'get input from user'

---
+++ day02/housework/little_kiosk.py
@@ -0,0 +1,16 @@
+print("Hello world!")
+
+print("지금 이 자판기에서는\n1.콜라\n2.사이다\n3.환타\n들을 판매하고있습니다.")
+coke = 1000
+soda = 1100
+fanta = 1200
+print("얼마를 넣으실건가요?\n>>",end="")
+input_cost = int(input())
+
+
+
+
+
+
+
+

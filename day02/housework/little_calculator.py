bdece53d1fbb69b33f2ed33a4d3fddf5cdc5c4d0
ghdwpaks <hjm7904@gmail.com>
2021-02-07 17:48:24 +0900

    made little calculator part 2 'print result'

--- day02/housework/little_calculator.py
+++ day02/housework/little_calculator.py
@@ -24,6 +24,22 @@ while(True) :
     else :
         print("입력이 잘못됐습니다.\n다시 입력해주세요.")
         continue
+while(True) :
+    print("무슨 계산을 하시겠습니까?\n>>",end='')
+    in1 = input()
+    print('in1 :' + in1)
+    if in1 == '덧셈' or  in1 == '합' or  in1 == '+':
+        res = a + b
+        break
+    elif  in1 == '뺄셈' or  in1 == '빼기' or  in1 == '마이너스' or  in1 == '-':
+        res = a - b
+        break
+    elif  in1 == '곱셈' or  in1 == '곱' or  in1 == '*':
+        res = a * b
+        break
+    elif  in1 == '나누기' or  in1 == '나눗셈' or  in1 == '/':
+        res = a / b
+        break
 print("결과는 " , res , "입니다.")
 
 

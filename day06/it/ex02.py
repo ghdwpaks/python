ccf9ee032cc9b39603e371aaef62f398465ae317
ghdwpaks <hjm7904@gmail.com>
2021-02-15 21:54:01 +0900

    std of day 06 'how to use if'

---
+++ day06/it/ex02.py
@@ -0,0 +1,9 @@
+d = input("정수입력 : ")
+d = int(d)
+
+
+if d > 10 :
+    print("{}은(는) 10부다 크다.".format(d))
+
+if d > 0 and d <= 100 :
+    print("{}는 1~100 범위 안에 정수입니다.".format(d))
---
+++ day06/it/ex04.py
@@ -0,0 +1,12 @@
+print("1. easy game")
+print("2. hard game")
+print("3. exit")
+
+in1 = int(input("선택 : "))
+
+if in1 == 1:
+    print("gamemode : easy")
+if in1 == 2:
+    print("gamemode : hard")
+if in1 == 3:
+    print("game exit")

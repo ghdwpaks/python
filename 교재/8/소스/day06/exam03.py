2086ecb86d3c90e039a42031e0fa14471378ceda
ghdwpaks <hjm7904@gmail.com>
2021-02-17 21:56:19 +0900

    std of day 08 'got text data of class'

---
+++ 교재/8/소스/day06/exam03.py
@@ -0,0 +1,13 @@
+
+print('1. easy game')
+print('2. hard game')
+print('3. exit')
+
+data = int(input('선택 : '))
+
+if data == 1 :
+    print('easy game start')
+if data == 2 : 
+    print('hard game start')
+if data == 3 :
+    print('game exit')
---
+++ 교재/8/소스/day07/exam04.py
@@ -0,0 +1,10 @@
+
+data = input('정수 입력 : ')
+data = int(data)
+
+if data > 100:
+    print('{}는 100보다 큰 수 입니다.'.format(data))
+elif data == 100:
+    print('{}는 100과 같은 수 입니다.'.format(data))
+elif data > 50 :
+     print('{}는 50보다 큰 수 입니다.'.format(data))

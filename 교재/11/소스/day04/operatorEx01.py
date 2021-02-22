3a08bc341fe66ba7e3323de5bdbc910304d08181
ghdwpaks <hjm7904@gmail.com>
2021-02-22 22:37:45 +0900

    std of day 10 'got text data from class'

---
+++ 교재/11/소스/day04/operatorEx01.py
@@ -0,0 +1,10 @@
+data1 = 9
+data2 = 2
+
+print('{} + {} = {}'.format(data1, data2, data1+data2))
+print('{} - {} = {}'.format(data1, data2, data1-data2))
+print('{} * {} = {}'.format(data1, data2, data1*data2))
+print('{} / {} = {}'.format(data1, data2, data1/data2))
+print('{} // {} = {}'.format(data1, data2, data1//data2))
+print('{} % {} = {}'.format(data1, data2, data1%data2))
+print('{} ** {} = {}'.format(data1, data2, data1**data2))

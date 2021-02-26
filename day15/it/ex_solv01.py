2d88f504071b2cf5bdd9c21c8166c860fabc61be
ghdwpaks <hjm7904@gmail.com>
2021-02-26 23:28:04 +0900

    std of day 15 'solved pros of class'

---
+++ day15/it/ex_solv01.py
@@ -0,0 +1,13 @@
+l = []
+for i in range(0,3) :
+    u = int(input("{}번째 정수 입력 : ".format(i+1)))
+    l.append(u)
+
+print("\n\n\n")
+
+for i in l :
+    print(i)
+
+
+'''선생님 풀이'''
+list_data = [0,0,0]

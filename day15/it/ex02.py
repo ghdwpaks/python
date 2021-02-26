a1f92fc230653d78558cc39882742959e7b5fac6
ghdwpaks <hjm7904@gmail.com>
2021-02-26 23:27:43 +0900

    std of day 15 'data of class day 15'

---
+++ day15/it/ex02.py
@@ -0,0 +1,12 @@
+list_data = [10,20,30,40]
+for i in list_data :
+    print("{} ".format(i))
+
+print("list_dta 길이 :",len(list_data))
+
+for i in range(len(list_data)) :
+    print("{} ".format(list_data[i]))
+
+
+
+    print()

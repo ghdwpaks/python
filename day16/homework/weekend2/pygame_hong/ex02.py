32fcf9a4ddb8ee1b6b2c0d0e0da69af60d628ad5
ghdwpaks <hjm7904@gmail.com>
2021-02-28 23:11:44 +0900

    studied about Self-referencing 자기참조

---
+++ day16/homework/weekend2/pygame_hong/ex02.py
@@ -0,0 +1,30 @@
+class fun:
+    def func1() :
+        print("function 1")
+    def func2(s) :
+        print("function 2")
+    def func3(s) :
+        print("function 3")
+        print("s :",s)
+        s.img = 'ghdwpaks'
+        print("s.img =",s.img)
+
+print("Hello world!")
+f = fun()
+print("/////")
+print("id(f) :",id(f))
+print("f :",f)
+f.func3()
+print("/////")
+fun.func2("ghd")
+fun.func3("ghd")
+print()
+fs = 'ghdwpaks'
+fun.func3(fs)
+
+
+
+
+
+
+            

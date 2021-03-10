4ddaf0d00e0df95c8a317354a1e4aeed680354cd
ghdwpaks <hjm7904@gmail.com>
2021-03-11 00:10:19 +0900

    made program binary calculator abrogated

--- binary_calculator/main.py
+++ binary_calculator/main.py
@@ -9,8 +9,15 @@ def get_cal_type() :
         elif u == "-" or u == "2" :
             return False
         
-
-
+def get_under(s) :
+    res = 0
+    if s[0] == 1 :
+        res += 0.5
+    elif s[1] == 1:
+        res += 0.25
+    elif s[2] == 1 :
+        res += 0.125
+    return res
 
 
 while True :
@@ -19,12 +26,22 @@ while True :
     u1 = u1.split(".")
     u2 = input("두번째 수 입력 : ")
     u2 = u1.split(".")
+    
     u3 = get_cal_type()
-    r1 = r2 = 0
+    r1 = r2 = res = 0
     if u3 :
         r1 = bin(u1[0]) + bin(u2[0])
+        tr1 = get_under(u1[1])
+        tr2 = get_under(u2[1])
+        r2 = tr1 + tr2
+        res = r1+r2
     else :
         r1 = bin(u1[0]) - bin(u2[0])
+        tr1 = get_under(u1[1])
+        tr2 = get_under(u2[1])
+        r2 = tr1 - tr2
+        res = r1+r2
+    print("결과 :",res)
 
 
 

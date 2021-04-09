92cbfa99f4040d1db9f020ddff12eacc32089658
ghdwpaks <hjm7904@gmail.com>
2021-04-09 12:07:20 +0900

    step 2 'added all data of economically active population'경제활동인구 총합

--- control_data/f1/m1.py
+++ control_data/f1/m1.py
@@ -6,5 +6,16 @@ with open('table_type1.csv','r') as f :
     reader = csv.DictReader(f)
     for row in reader :
         t.append(row)
-        print(row)
+        #print(row)
 print("Hello world!")
+#경제 활동 인구 economically active population
+#def eap_t () :
+sum = 0.0
+for i in range(len(t)) :
+    
+    if t[i]['3'] == '-' :
+        pass
+    else : 
+        print(t[i]['3']+"+",end="")
+        sum += float(t[i]['3'])
+print("{:.2f}".format(sum))

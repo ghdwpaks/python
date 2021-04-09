d7fb701f7864bf8cca08de4d358400a195b10cb7
ghdwpaks <hjm7904@gmail.com>
2021-04-09 18:38:54 +0900

    step 4 'print rank of city economically active population' 도시별 경제활동인구 순위값 구하기

--- control_data/f1/m1.py
+++ control_data/f1/m1.py
@@ -21,13 +21,41 @@ for i in range(len(t)) :
 print("{:.2f}".format(sum))
 print("\n\n\n\n")
 temp = []
+count_trash = 0
 for i in range(len(t)) :
     
     if t[i]['3'] == '-' :
-        pass
+        count_trash += 1
     else : 
         temp.append(float(t[i]['3']))
-print(temp)
+#print(temp)
 temp.sort(reverse=True)
 #print("\n\n\n\n")
+'''
 print(temp[0])
+for i in range(len(temp)) :
+    print("{}   {}".format(temp[5],t[i]['3']))
+'''
+target = 0
+pas = True
+while pas :
+    for i in range(len(t)) :
+        if t[i]['3'] == '-' :
+            pass
+        else : 
+            
+            #print(temp[5] , "" ,t[i]['3'])
+            if float(t[i]['3']) == temp[target] :
+                print("경제횔동인구가 {}번째로 높은 도시는 \t{} 로 \t{} 천명입니다.".format(target+1,t[i]['1'],t[i]['3']))
+                '''
+                print(target + 1)
+                print(len(t) - count_trash)
+                '''
+                target += 1 
+                if target >= (len(t) - count_trash) :
+                    pas = False
+                    break
+                #print(True)
+            else :
+                #print(False)
+                pass

d2d76683599398b8e7e27a20490089af70ab549b
ghdwpaks <hjm7904@gmail.com>
2021-04-10 22:05:29 +0900

    step 6 'structuring show rank' 컨트롤 데이터 구조화 2단계 순위 보여주기

--- control_data/f1/m1.py
+++ control_data/f1/m1.py
@@ -12,46 +12,78 @@ count_trash = 0
 temp = []
 lent = 0
 target = 0
+subject1 = ""
+subject2 = ""
 def set_rank() :
-    global target,lent,count_trash
-    print("target =",target)
+    global subject1,subject2 ,target,lent,count_trash
+    print("1target =",target)
     print("type target =",type(target))
     for i in range(len(t)) :
         
-        if t[i]['3'] == '-' :
+        if t[i][str(target)] == '-' :
             count_trash += 1
         else : 
-            temp.append(float(t[i]['3']))
+            temp.append(float(t[i][str(target)]))
     temp.sort(reverse=True)
     lent = int(len(t) - count_trash)
+    if target == 2 :
+        subject1 = "15세이상인구";subject2 = "천명"
+    elif target == 3:
+        subject1 = "경제활동인구";subject2 = "천명"
+    elif target == 4:
+        subject1 = "취업자";subject2 = "천명"
+    elif target == 5:
+        subject1 = "실업자";subject2 = "천명"
+    elif target == 6:
+        subject1 = "실업자";subject2 = "C.V."
+    elif target == 7:
+        subject1 = "비경제활동인구";subject2 = "천명"
+    elif target == 8:
+        subject1 = "경제활동참가율";subject2 = "％"
+    elif target == 9:
+        subject1 = "고용률";subject2 = "％"
+    elif target == 10:
+        subject1 = "15~64세 고용률";subject2 = "％"
+    elif target == 11:
+        subject1 = "실업률";subject2 = "％"
+    elif target == 12:
+        subject1 = "실업률";subject2 = "C.V."
+
 def get_total() :
     sum = 0.0
     global target
-    target = str(target)
     for i in range(len(t)) :
         
-        if t[i][target] == '-' :
+        if t[i][str(target)] == '-' :
             pass
         else : 
-            print(t[i][target]+"+",end="")
+            print(t[i][str(target)]+"+",end="")
             sum += float(t[i][target])
     print("{:.2f}".format(sum))
 def show_rank() :
-    global target
+    global target,temp,lent,count_trash
+    Rotating_target = 1
+    print("2target =",Rotating_target)
+    print("type target =",type(Rotating_target))
+    
     pas = True
     while pas :
         for i in range(len(t)) :
-            if t[i]['3'] == '-' :
+            #print(t[i][str(target)])
+            if t[i][str(target)] == '-' :
                 pass
             else : 
-                if float(t[i]['3']) == temp[target] :
-                    print("경제횔동인구가 {}번째로 높은 도시는 \t{} 로 \t{} 천명입니다.".format(target+1,t[i]['1'],t[i]['3']))
-                    target += 1 
-                    if target >= lent :
+                
+                if float(t[i][str(target)]) == temp[Rotating_target] :
+                    print("{}(이)가 {}번째로 높은 도시는 \t{} 로 \t{} {}입니다.".format(subject1,Rotating_target+1,t[i]['1'],t[i][str(target)],subject2))
+                    Rotating_target += 1 
+                    if Rotating_target >= lent :
                         pas = False
                         break
-target = 1
+#print(t)
+target = 9
 set_rank()
-print(temp)
+#print(temp)
+show_rank()
 
 

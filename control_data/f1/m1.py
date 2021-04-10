dbadb4e133689c14cb32d245678babb4c79d85d8
ghdwpaks <hjm7904@gmail.com>
2021-04-10 23:03:38 +0900

    step 8 'made ui for use easly' 컨트롤 데이터 구조화 4단계 편의목적 사용자 사용환경만들기 !!need modify 추가수정필요함!!

--- control_data/f1/m1.py
+++ control_data/f1/m1.py
@@ -27,7 +27,7 @@ def set_rank() :
     temp.sort(reverse=True)
     lent = int(len(t) - count_trash)
     if target == 2 :
-        subject1 = "15세이상인구";subject2 = "천명"
+        subject1 = "15세이상인구 ";subject2 = "천명"
     elif target == 3:
         subject1 = "경제활동인구";subject2 = "천명"
     elif target == 4:
@@ -61,6 +61,7 @@ def get_total() :
             sum += float(t[i][str(target)])
     print("{:.2f}".format(sum))
     print("{}의 총합은{:.1f}{}입니다.".format(subject1,sum,subject2))
+    os.system("pause")
     return sum
 def show_rank() :
     global target,temp,lent,count_trash
@@ -82,12 +83,62 @@ def show_rank() :
                     if Rotating_target >= lent :
                         pas = False
                         break
+    os.system("pause")
+
+def choose_func(subject_main) :
+    global target
+    p2 = True
+    target = subject_main
+    set_rank()
+    while p2 :
+        os.system("cls")
+        print("0.뒤로가기")
+        print("1.순위별로 출력")
+        if subject_main == 7 or subject_main == 8 or subject_main == 9 or subject_main == 10 or subject_main == 11 :
+            pass
+        else :
+            print("2.총합 출력")
+        u2 = input("종류선택 : ")
+        if "exit" == u2 or "종료" == u2 or '0' == u2 or "EXIT" in u2:
+            p2 = False
+        elif u2 == '' :
+            continue
+        elif int(u2) == 1 :
+            show_rank()
+        elif int(u2) == 2 :
+            get_total()
+        else :
+            continue
+'''
 #print(t)
 target = 5
 set_rank()
 #print(temp)
 #show_rank()
 res1 = get_total()
-
-
-
+'''
+print("Hello world!")
+print("")
+p = True
+while p :
+    os.system("cls")
+    print("1.15세이상인구")
+    print("2.경제활동인구")
+    print("3.취업자")
+    print("4.실업자")
+    print("5.실업자(C.V.)")
+    print("6.비경제활동인구")
+    print("7.경제활동참가율(%)")
+    print("8.고용률(%)")
+    print("9.15~64세 고용률(%)")
+    print("10.실업률(%)")
+    print("11.실업률(C.V.)")
+    print()
+    print("0.EXIT 종료")
+    u = input("종류선택 : ")
+    if "exit" == u or "종료" == u or '0' == u or "EXIT" in u:
+        p = False
+    elif int(u) < 12 and int(u) > 0 :
+        choose_func(int(u)+1)
+    else :
+        continue

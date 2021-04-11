effa6ad043a01b91b93e5eb471ab69f3aaf34647
ghdwpaks <hjm7904@gmail.com>
2021-04-11 22:14:15 +0900

    step 9 plus 1'make function of feach from rank' 컨트롤 데이터 구조화 5단계 도시이름으로 순위 및 데이터 검색해서 가져오기 수정 1

--- control_data/f1/m2.py
+++ control_data/f1/m2.py
@@ -16,10 +16,8 @@ subject1 = ""
 subject2 = ""
 def set_rank() :
     global subject1,subject2 ,target,lent,count_trash
-    '''
     print("1target =",target)
     print("type target =",type(target))
-    '''
     for i in range(len(t)) :
         
         if t[i][str(target)] == '-'or t[i][str(target)] == '*' :
@@ -63,16 +61,15 @@ def get_total() :
             sum += float(t[i][str(target)])
     print("{:.2f}".format(sum))
     print("{}의 총합은{:.1f}{}입니다.".format(subject1,sum,subject2))
-    os.system("pause")
-    #print("\n\n\n")
+    #os.system("pause")
+    print("\n\n\n")
     return sum
 def show_rank() :
     global target,temp,lent,count_trash
     Rotating_target = 0
-    '''
     print("2target =",Rotating_target)
     print("type target =",type(Rotating_target))
-    '''
+    
     pas = True
     while pas :
         for i in range(len(t)) :
@@ -87,17 +84,16 @@ def show_rank() :
                     if Rotating_target >= lent :
                         pas = False
                         break
-    os.system("pause")
-    #print("\n\n\n")
+    #os.system("pause")
+    print("\n\n\n")
 
 def show_seach() :
     global target,temp,lent,count_trash
     Rotating_target = 0
     keyword = input("도시 이름 입력 : ")
-    '''
     print("2target =",Rotating_target)
     print("type target =",type(Rotating_target))
-    '''
+    
     pas = True
     while pas :
         for i in range(len(t)) :
@@ -106,12 +102,13 @@ def show_seach() :
                 pass
             else : 
                 
-                if t[i]['1'] == keyword :
-                    print("{}는 {}부분에서 {}위로 값은 {}{}입니다.".format(keyword,subject1,temp.index(float(t[i][str(target)]))+1,t[i][str(target)],subject2))
+                if float(t[i]['1']) == keyword :
+                    print("{}는 {}부분에서 {}위로 값은 {}{}입니다.".format(keyword,subject1,i+1,t[i][target],subject2))
                     pas = False
+                    
                     break
-    os.system("pause")
-    #print("\n\n\n")
+    #os.system("pause")
+    print("\n\n\n")
 
 
 def choose_func(subject_main) :
@@ -120,15 +117,14 @@ def choose_func(subject_main) :
     target = subject_main
     set_rank()
     while p2 :
-        os.system("cls")
-        #print("\n\n\n")
+        #os.system("cls")
+        print("\n\n\n")
         print("0.뒤로가기")
         print("1.순위별로 출력")
-        print("2.특정도시순위검색")
         if subject_main == 7 or subject_main == 8 or subject_main == 9 or subject_main == 10 or subject_main == 11 :
             pass
         else :
-            print("3.총합 출력")
+            print("2.총합 출력")
         u2 = input("종류선택 : ")
         if "exit" == u2 or "종료" == u2 or '0' == u2 or "EXIT" in u2:
             p2 = False
@@ -137,42 +133,11 @@ def choose_func(subject_main) :
         elif int(u2) == 1 :
             show_rank()
         elif int(u2) == 2 :
-            show_seach()
-        elif int(u2) == 3 :
             get_total()
         else :
             continue
-'''
+
 #print(t)
 target = 5
 set_rank()
-#print(temp)
-#show_rank()
-res1 = get_total()
-'''
-print("Hello world!")
-print("")
-p = True
-while p :
-    os.system("cls")
-    #print("\n\n\n")
-    print("1.15세이상인구")
-    print("2.경제활동인구")
-    print("3.취업자")
-    print("4.실업자")
-    print("5.실업자(C.V.)")
-    print("6.비경제활동인구")
-    print("7.경제활동참가율(%)")
-    print("8.고용률(%)")
-    print("9.15~64세 고용률(%)")
-    print("10.실업률(%)")
-    print("11.실업률(C.V.)")
-    print()
-    print("0.EXIT 종료")
-    u = input("종류선택 : ")
-    if "exit" == u or "종료" == u or '0' == u or "EXIT" in u:
-        p = False
-    elif int(u) < 12 and int(u) > 0 :
-        choose_func(int(u)+1)
-    else :
-        continue
+show_seach

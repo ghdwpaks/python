d001718919657391de301027e72c5228ec987194
ghdwpaks <hjm7904@gmail.com>
2021-05-04 23:45:00 +0900

    f3 step 3 made func show ops of country '기초적인 국가 LFS 정보 출력 기능 제작'

--- control_data/f3/main.py
+++ control_data/f3/main.py
@@ -8,26 +8,8 @@ print("Hello world!")
 country_names = ['그리스','네덜란드','노르웨이','뉴질랜드','덴마크','독일','라트비아','러시아','룩셈부르크','리투아니아','멕시코','미국','벨기에','스웨덴','스위스','스페인','슬로바키아','슬로베니아','아이슬란드','아일랜드','에스토니아','영국','오스트레일리아','오스트리아','이스라엘','이탈리아','일본','체코','칠레','캐나다','콜롬비아','터키','포르투갈','폴란드','프랑스','핀란드','한국','헝가리']
 genders = ["남자",'여자','개인 전체']
 
-temp = [] #temp, 임시 테이블
-country = [] #전체 중 나라 구분
-gendger = [] #gender, 나라 중 성별구분
-age = [] #age, 성별 중 나이 구분
-def remove_zero(string) :
-    #들어온 변수에 대해서, 0을 제거해주는 변수
-    string = str(string).strip()
-    print("string :",string)
-    temp_string = list(string)
-    i = 0
-    while True :
-        i += 1
-        if i < len(temp_string):
-            if temp_string[i] == '0' :
-                del temp_string[i]
-        else :
-            break
-    string = ''.join(temp_string)
-    print(string)
-    return int(string)
+table = []
+temp = []
 def check_country_section(country_name) :
     #나라 이름 받고서 country_names에 해당하는 위치(상수)를 반환함.
     global country_names
@@ -47,76 +29,64 @@ def check_gender_section(gender_name) :
 
 def setting_table_level_1() :
     #1단계, 테이블 불러오기  
-    global temp
-    with open('table.csv','r') as f :
+    global table
+    with open('table2.csv','r') as f :
         reader = csv.DictReader(f)
         for row in reader :
-            temp.append(row)
+            table.append(row)
             #print(row)      
 
 def setting_table_level_2() :
     #2단계, 각 나라 위치에 맞는 곳에 국가 데이터 append 하기
-    global temp, country
-    for i in range(0,len(temp)) :
-        #print("i :",i)
-        temp_type = ["성별",'연령별','고용/인구 비율','노동참여 비율','실업률']
-        temp_for_appending = dict({})
-        #print("temp_for_appending :",temp_for_appending)
-        for j in range(len(temp_type)) :
-            #print("j :",j)
-            temp_for_appending[str(temp_type[j])] = temp[i][str(temp_type[j])]
-        #print("temp_for_appending :",temp_for_appending)
-        country[check_country_section(temp[i]['국가'])].append(temp_for_appending)
+    global temp
+    
+def show_all_country_names() :
+    for i in range(0,len(country_names)//5) :
+        for j in range(0,5) :
+            print(country_names[i*5+j],end="\t")
+            if len(country_names[i*5+j]) < 4 :
+                print("\t",end="")
+        print("")
+    for i in range(0,len(country_names)%5) :
+        print(country_names[-i],end="\t")
+        if len(country_names[-i]) < 4 :
+            print("\t",end="")
+
+def show_country_ops_country(country_name) :
+    global table
+    for i in range(len(table)) :
+        if table[i]['1'] == country_name :
+            print("국가 : {}".format(country_name))
+            print("\t성별 : {}".format(table[i]['2']))
+            print("\t\t연령 : {}".format(table[i]['3']))
+            print("\t\t\t고용인구비율 : {}".format(table[i]['4']))
+            print("\t\t\t노동참여비율 : {}".format(table[i]['5']))
+            print("\t\t\t실업률 : {}".format(table[i]['6']))
+
+def show_country_ops_center() :
+    #나라 정보 출력 기능
+    global country_names
+    print("출력 가능 국가는 다음과 같습니다.")
+    show_all_country_names()
+    ans1_country = ""
+    while True :
+        ans1_country = input("\t국가입력 : ")
+        if ans1_country in country_names :
+            break
+        else :
+            print("해당 국가는 포함되지 않았습니다.")
+    show_country_ops_country(ans1_country)
         
-        #temp[i]['성별'],temp[i]['연령별'],temp[i]['고용/인구 비율'],temp[i]['노동참여 비율'],temp[i]['실업률']
-
-def setting_table_level_3() :
-    #3단계, 각 나라 위치에 맞는 곳에 성별 데이터 append 하기
-    global country,gendger
-    for i in range(0,len(country)) :
-        for j in range(0,len(country[i])) :
-            temp_type = ['연령별','고용/인구 비율','노동참여 비율','실업률']
-            temp_for_appending = []
-            for k in range(0,len(temp_type)) :
-                temp_for_appending.append(country[i][j][temp_type[k]])
-            
-            
-
-
-def setting_country_list() :
-    global country
-    for i in range(len(country_names)) :
-        country.append([])
-def setting_gender_list() :
-    global gendger
-    for i in range(len(genders)) :
-        gendger.append([])
-
-
-'''
-def setting_gender_list() :
-    global gendger, gendgers
-    for i in range(len(gendgers)) :
-        gendger.append([])
-'''
-            
+    
+    
+ 
 
 print("Hello world!")
-a = check_country_section("칠레")
-print(a)
-setting_country_list()
 setting_table_level_1()
-#print("len(temp) :",len(temp))
-#print("1",temp)
-setting_table_level_2()
-print(len(country))
-'''
-printing = str(country)
-for i in range(0,100) :
-    print("\n")
-
-for i in range(len(printing)) :
-    print(printing[i],end='')
-    if printing[i] == "]" or printing[i] == "}" :
-        print("")
-'''
+#show_all_country_names()
+while True :
+    print("OECD 가입국가의 LFS입니다.")
+    print("1.국가의 모든 LFS 정보 출력")
+    ans_main_1 = input("종류선택 : ")
+    if ans_main_1 == '1' :
+        show_country_ops_center()

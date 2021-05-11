3f0de982f3d36c69e33ce024cbbc52660f64e864
ghdwpaks <hjm7904@gmail.com>
2021-05-11 23:23:41 +0900

    f4 step 1 made func setting subjects '종목 출력 및 반환 기능 제작'

--- control_data/f4/main.py
+++ control_data/f4/main.py
@@ -7,10 +7,67 @@ with open('table.csv','r') as f :
     for row in reader :
         table.append(row)
         #print(row)
-'''
+
 a = str(table)
 for i in range(len(a)) :
     print(a[i],end='')
     if a[i] == "}" :
         print("")
-'''
+
+def setting_product_short_name() :
+    global table
+    names_product = []
+    for i in range(len(table)) :
+        if table[i]['1'] not in names_product :
+            names_product.append(table[i]['1'])
+    #print(names_product)
+
+    names_product2 = []
+    for i in range(len(names_product)) :
+        names_product2.append(c.deepcopy(names_product[i].split(']')[0]))
+
+    for i in range(len(names_product2)) :
+        names_product2[i] = names_product2[i] + ']'
+
+    names_product3 = []
+    for i in range(len(names_product2)) :
+        if names_product2[i] not in names_product3 :
+            #print(names_product2[i])
+            names_product3.append(names_product2[i])
+    return names_product3
+def setting_product_long_name() :
+    global table
+    names_product = []
+    for i in range(len(table)) :
+        if table[i]['1'] not in names_product :
+            names_product.append(table[i]['1'])
+    return names_product
+
+
+
+
+
+def integrate_subjects() :
+    global table
+    names_product = setting_product_long_name()
+    temp = []
+    temp_keys = []
+    for i in range(len(names_product)) :
+        for j in range(len(table)) :
+
+            if table[j]['1'] == names_product[i] :
+                temp.append(table[j])
+                
+
+
+
+
+
+    
+count = 0 
+for i in range(len(table)) :
+    if table[i]['3'] == "특(1등)" :
+        count += 1
+    
+print(count)
+print(len(table))
